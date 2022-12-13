3b35b73c28f2f0a00478b66009e86f89904fd725
deji100 <adeyemidej9@gmail.com>
2022-12-13 08:31:24 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry
+ * Return: Always 0
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/* function prints alphabets in lowercase */
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n")
+}
