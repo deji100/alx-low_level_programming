4cddda594e9d825c1b2d87b0d0e28e19a4990061
deji100 <adeyemidej9@gmail.com>
2022-12-13 19:10:17 -0800

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	char letter;
 	int n;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (n = 'a'; n < 10; n++)
 
 	{
-		for (n = 0; n < 10; n++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
